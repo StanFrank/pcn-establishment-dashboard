a28b84fd595a1b1560c3c2b9c562f1aaa8e57a96
StanFrank <103993206+StanFrank@users.noreply.github.com>
2025-11-02 18:38:36 +0300

    Update dashboard2.1.py

--- dashboard2.1.py
+++ dashboard2.1.py
@@ -335,6 +335,23 @@ with col2:
             mapbox=dict(bearing=0, pitch=0),
             margin={"r": 0, "t": 0, "l": 0, "b": 0},
         )
+        fig_map.update_layout(
+            coloraxis_colorbar=dict(
+                title=selected_indicator,
+                tickformat=".0f",
+                title_side="top",
+                x=0.95,          # horizontal position (0=left, 1=right)
+                xanchor="right",
+                y=0.5,            # vertical position (0=bottom, 1=top)
+                yanchor="middle",
+                len=0.5,          # length of the colorbar (0–1 range)
+                thickness=12,     # thickness of the colorbar
+                bgcolor="rgba(255,255,255,0.7)",  # background to make it readable
+                outlinecolor="black",
+                outlinewidth=1,
+            )
+
+        
         fig_map.add_annotation(
             text=f"{selected_indicator} by County",
             xref="paper",
@@ -366,6 +383,7 @@ st.header("County Data Table")
 # Use the filtered pillar_df for the table
 
 st.dataframe(pillar_df.sort_values(by='County'), use_container_width=True)
+
 
 
 
